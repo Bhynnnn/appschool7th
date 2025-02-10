7d585c737e8fd051c85d48f4ea758c7509730579
bhyn <bhyn9785@gmail.com>
2025-02-10 10:03:20 +0900

    commit message

---
+++ SwiftUI/MoveLog/MoveLog/MoveLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveLogApp.swift
+//  MoveLog
+//
+//  Created by 강보현 on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MoveLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
